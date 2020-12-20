7ac900924cca716bdf7219ca6461ae58187a6047
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-12-19 21:32:59 -0500

    form addition

--- employee-directory/src/components/FormInput.js
+++ employee-directory/src/components/FormInput.js
@@ -3,4 +3,23 @@ import React from "react";
 function FormInput(props) {
     console.log(props);
     //form to search
+    return (
+        <div>
+            <form className="form">
+                <input
+                    value={props.firstName}
+                    type="text"
+                    placeholder="First Name"
+                />
+                <input
+                    value={props.lastName}
+                    type="text"
+                    placeholder="Last Name"
+                />
+                <button onClick={props.handleFormSubmit}>Submit</button>
+            </form>
+        </div>
+    );
 }
+
+export default FormInput;
--- employee-directory/src/components/UsersView.js
+++ employee-directory/src/components/UsersView.js
@@ -2,4 +2,9 @@ import React from "react";
 
 function UsersView(props) {
     //render employees
+    <div>
+        <RenderEmployees employees={props.employees} />
+    </div>
 }
+
+export default UsersView;
