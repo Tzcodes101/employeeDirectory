2092962cafdc1fc4b1cfd328bca9fd00a383d7b5
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-12-20 10:09:46 -0500

    table body addition

--- employee-directory/src/components/RenderEmployees.js
+++ employee-directory/src/components/RenderEmployees.js
@@ -4,44 +4,44 @@ function RenderEmployees(props) {
     //render employees in a table
     return (
 
-            <div className="data-table">
-                <table id="table">
-                    <thead>
-                        <tr>
-                            <th className="col">Image</th>
-                            <th className="col">Name</th>
-                            <th className="col">Email</th>
-                            <th className="col">Phone</th>
-                            <th className="col">DOB</th>
-                        </tr>
-                    </thead>
+        <div className="data-table">
+            <table id="table">
+                <thead>
+                    <tr>
+                        <th className="col">Image</th>
+                        <th className="col">Name</th>
+                        <th className="col">Email</th>
+                        <th className="col">Phone</th>
+                        <th className="col">DOB</th>
+                    </tr>
+                </thead>
 
-                    <tbody>
+                <tbody>
 
-                        {props.employees.map((employee, index) => {
-                            return (
-                            <div key={index}>
-                                <div className="img-container">
-                                    <img alt={employee.name.first} src={employee.picture.thumbnail} />
-                                </div>
-                                <div className="info">
-                                    <ul style={{ listStyleType: "none" }}>
-                                        <li>
-                                            Name: {employee.name.first} {employee.name.last}
-                                        </li>
-                                        <li>
-                                            Email: {employee.email}
-                                        </li>
-                                        <li>
-                                            Phone: {employee.cell}
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        )})}
-                    </tbody>
-                </table>
-            </div>
+                    {props.employees.map((employee, index) => {
+                        return (
+                            <tr key={index}>
+                                <td className="img-container">
+                                    <img alt={employee.name.first} src={employee.picture.medium} />
+                                </td>
+                                <td className="employee-name">
+                                    {employee.name.first} {employee.name.last}
+                                </td>
+                                <td className="email">
+                                    {employee.email}
+                                </td>
+                                <td className="phone">
+                                    {employee.cell}
+                                </td>
+                                <td className="DOB">
+                                    {employee.DOB}
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
 
     )
 
